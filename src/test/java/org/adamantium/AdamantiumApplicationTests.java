fa99b09897cf744dc01d6aacae286afc27f5f576
Vatsal Sura <vatsalhsura@gmail.com>
2022-04-14 01:25:01 +0530

    init

---
+++ src/test/java/org/adamantium/AdamantiumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adamantium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdamantiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
